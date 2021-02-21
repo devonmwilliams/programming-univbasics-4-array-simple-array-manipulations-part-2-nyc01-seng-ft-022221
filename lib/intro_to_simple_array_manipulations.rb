26e34368cfe5eaf9e6e756c3bd07df3fa57f870f
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-21 11:47:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,10 +1,25 @@
 require 'pry'
 
-def using_concat(my_favorite_things, more_favs)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["sports cars", "flatiron school"]
-  all_my_favs = my_favorite_things.concat(more_favs)
-all_my_favs
+def using_concat(array1, array2)
+  array1.concat(array2)
 end
 
-def using_insert(array)
+def using_insert(array, object)
+  array.insert(2, object)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, number)
+  array.delete_at(number)
+end
